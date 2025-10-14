e3ea3ad07fb5b97577fa60df19703fe17ee08bea
Cauã Grigolatto <cauagrigolatto23@gmail.com>
2025-10-14 10:23:31 -0300

    Upload project's initial structure with Spring Boot and React

---
+++ sysodonto/src/test/java/br/edu/ifsp/sysodonto/SysodontoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.sysodonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysodontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
