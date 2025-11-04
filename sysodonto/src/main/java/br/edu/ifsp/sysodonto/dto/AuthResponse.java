94b90cb6b17edb160584af2c7dc939412a613f47
João Pedro da Silva Vieira AQ3022366 <vieira.joao1@aluno.ifsp.edu.br>
2025-11-04 20:04:00 -0300

    making auth response

---
+++ sysodonto/src/main/java/br/edu/ifsp/sysodonto/dto/AuthResponse.java
@@ -0,0 +1,22 @@
+package br.edu.ifsp.sysodonto.dto;
+
+public class AuthResponse {
+
+    private String token;
+
+    public AuthResponse(String token){
+        this.token = token;
+    }
+
+    public AuthResponse(){
+        
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
